0770a942acef782bd467f6b9f65a64dba43e4403
Dheeraj Kandpal <dheeraj.kandpal89@gmail.com>
2019-10-23 00:08:45 -0700

    Couroutine Test with navigation fragment

--- app/src/main/java/com/example/mynavigationtest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mynavigationtest/ui/home/HomeFragment.kt
@@ -8,14 +8,24 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigator
 import androidx.navigation.findNavController
 import com.example.mynavigationtest.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private val RESULT_1="this is result 1"
+    private val RESULT_2="this is result 2"
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +42,16 @@ class HomeFragment : Fragment() {
 
         root?.button_to_dashboard?.setOnClickListener(View.OnClickListener {
 
-            it.findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard)
+            // val action = HomeFragment.actionMessageListFragmentToMessageFragment(userRepository.getSignedInUser().userId)
 
+            //it.findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard)
+//Couroutine scope IO=For network and database operation,Main,Default=For heavy Computation
+
+            CoroutineScope(IO).launch {
+
+
+                fakeApiRequest()
+            }
 
         })
 
@@ -45,4 +63,43 @@ class HomeFragment : Fragment() {
         })
         return root
     }
+
+    private suspend fun fakeApiRequest(){
+
+        val result=getResultFromApi()
+        println("debug ${result}")
+
+        withContext(Main){
+
+            var txt=text_home.text.toString()+"\n "+result
+            text_home.text=txt
+
+        }
+        val result2=getResultFromApi2()
+        withContext(Main){
+
+            var txt=text_home.text.toString()+"\n "+result2
+            text_home.text=txt
+
+        }
+    }
+
+    private suspend fun getResultFromApi2():String{
+
+        logThread("getResultFromApi2")
+        delay(3000)
+        return RESULT_2
+    }
+
+    private suspend fun getResultFromApi():String {
+
+        logThread("getResultFromApi")
+        delay(2000)
+        return RESULT_1
+    }
+
+    private fun logThread(methodName: String) {
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    }
+
 }
--- app/src/main/java/com/example/mynavigationtest/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/mynavigationtest/ui/notifications/NotificationsFragment.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.mynavigationtest.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_notifications.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
+    private val PROGRESS_MAX=100
+    private val PROGRESS_START=0
+    private val JOB_TIME=4000
+    private lateinit var job:CompletableJob
+  lateinit  var textView: TextView
+    lateinit var button: Button
+    lateinit var progressBar: ProgressBar
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +38,114 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
+         textView = root.findViewById(R.id.text_notifications)
+        button = root.findViewById(R.id.button)
+        progressBar = root.findViewById(R.id.progressBar)
         notificationsViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+
+        button.setOnClickListener(View.OnClickListener {
+
+            if(!::job.isInitialized){
+
+                initJob()
+            }
+            progressBar.startJobOrCancel(job)
+
+        })
         return root
+    }
+
+
+    fun ProgressBar.startJobOrCancel(job : Job){
+
+
+        if(this.progress>0){
+
+            println("${job} is already active. Cancelling ...")
+            resetJob()
+        }
+        else{
+            button.text="Cancel Job #1"
+        val scope=    CoroutineScope(IO +job).launch {
+
+
+            println("couroutine ${this} is activated with job ${job}")
+
+            for(i in PROGRESS_MAX..PROGRESS_MAX)
+            {
+
+                delay((JOB_TIME/PROGRESS_MAX).toLong())
+                this@startJobOrCancel.progress=i
+            }
+            updateJobCompleteTextView("Job is Complete")
+            }
+        }
+
+    }
+
+    fun updateJobCompleteTextView(text:String){
+
+
+        GlobalScope.launch(
+            Main
+
+        ){
+
+            textView.text=text
+        }
+    }
+
+    private fun resetJob() {
+
+
+        if(job.isActive||job.isCompleted){
+
+            job.cancel(CancellationException(("Resetting Job")))
+
+
+        }
+        initJob()
+
+    }
+
+    fun initJob(){
+
+updateJobCompleteTextView("")
+        button.text="Start Job #1"
+        job= Job()
+        job.invokeOnCompletion {
+
+            it?.message.let{
+
+                var msg=it
+                if(msg.isNullOrBlank()){
+
+                    msg="Unknown cancellation Error"
+                }
+
+                println("${job} was cancelled. Reason : ${msg}")
+
+                showToast(msg)
+            }
+        }
+
+
+
+        progressBar.max=PROGRESS_MAX
+        progressBar.progress=PROGRESS_START
+    }
+
+
+    fun showToast(text:String){
+
+        GlobalScope.launch (Main){
+
+            Toast.makeText(activity,text,Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
